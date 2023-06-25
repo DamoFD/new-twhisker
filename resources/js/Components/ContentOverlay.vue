a6798be089b617fafd90ba6edb0fcc707e709154
DamoFD <voshall@yahoo.com>
2023-06-24 21:57:01 -0700

    dynamic user content

--- resources/js/Components/ContentOverlay.vue
+++ resources/js/Components/ContentOverlay.vue
@@ -5,8 +5,9 @@ import Heart from 'vue-material-design-icons/Heart.vue';
 import Comment from 'vue-material-design-icons/Comment.vue';
 
 defineEmits(['selectedPost'])
-const props = defineProps(['postByUser'])
+const props = defineProps(['postByUser', 'user'])
 const { postByUser } = toRefs(props);
+const { user } = toRefs(props);
 
 let isHover = ref(false);
 </script>
@@ -16,6 +17,7 @@ let isHover = ref(false);
         @mouseenter="$event => isHover = true"
         @mouseleave="$event => isHover = false"
         class="flex items-center justify-center cursor-pointer relative"
+        @click="$event => $emit('selectedPost', postByUser)"
     >
         <div
             v-if="isHover"
@@ -25,19 +27,19 @@ let isHover = ref(false);
             <div class="flex items-center justify-around w-[50%]">
                 <div class="flex items-center justify-center">
                     <Heart fillColor="#FFFFFF" :size="30" />
-                    <div class="pl-1">3</div>
+                    <div class="pl-1">{{ postByUser.likes.length }}</div>
                 </div>
                 <div class="flex items-center justify-center">
                     <Comment fillColor="#FFFFFF" :size="30" />
-                    <div class="pl-1">5</div>
+                    <div class="pl-1">{{ postByUser.comments.length }}</div>
                 </div>
             </div>
         </div>
 
         <img
             class="aspect-square mx-auto object-cover cursor-pointer"
-            src="https://picsum.photos/id/50/800/800"
-            alt="Post"
+            :src="postByUser.file"
+            :alt="'Post by ' + user.name"
         >
     </div>
 </template>
